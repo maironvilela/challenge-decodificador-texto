2334dcc1279f04faf289102adf75a3e362d2374c
mairon.vilela <maironvilela@hotmail.com>
2024-07-21 18:45:09 -0300

    refactor app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ function handleEncrypt() {
     event.preventDefault()
   })
 
-  const inputText = document.getElementById('input-text')
+  const inputText = document.getElementById('form__input-text')
   const hashMessage = encrypt(inputText.value)
 
-  const outputText = document.getElementById('output_text')
+  const outputText = document.getElementById('not-empty-message__output-text')
   outputText.innerHTML = hashMessage
 
   hiddenElementById('container__empty-message')
@@ -19,7 +19,7 @@ function encrypt(plainMessage) {
   const messageArray = plainMessage.split('')
 
   const letterArray = ['a', 'e', 'i', 'o', 'u']
-  const letterEncrypt = {
+  const characterToEncryptedToken = {
     a: 'ai',
     e: 'enter',
     i: 'imes',
@@ -27,21 +27,126 @@ function encrypt(plainMessage) {
     u: 'ufat',
   }
   const result = messageArray.map((letter) => {
-    return letterArray.includes(letter) ? letterEncrypt[letter] : letter
+    return letterArray.includes(letter)
+      ? characterToEncryptedToken[letter]
+      : letter
   })
 
   return result.join('')
 }
 
+function hiddenElementById(id) {
+  const element = document.getElementById(id)
+  element.classList.add('hidden')
+}
+
+function showElementById(id) {
+  const element = document.getElementById(id)
+  element.classList.remove('hidden')
+}
+
+function handleDecrypt() {
+  document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault()
+  })
+
+  const inputText = document.getElementById('form__input-text')
+  const hashMessage = inputText.value
+
+  const charactersArray = ['a', 'e', 'i', 'o', 'u']
+  let encryptedTextInArray = hashMessage.split('')
+  const result = rec(encryptedTextInArray, charactersArray, '')
+
+  const outputText = document.getElementById('not-empty-message__output-text')
+  outputText.innerHTML = result
+
+  hiddenElementById('container__empty-message')
+  showElementById('container__not-empty-message')
+}
+
+function rec(array, charactersArray, result) {
+  const characterToEncryptedToken = {
+    a: 'ai',
+    e: 'enter',
+    i: 'imes',
+    o: 'ober',
+    u: 'ufat',
+  }
+
+  let character = ''
+  let token = ''
+  let letterAndSplice = {}
+
+  if (array.length === 0) {
+    return result
+  }
+
+  if (!charactersArray.includes(array[0])) {
+    result += array[0]
+    array.splice(0, 1)
+    return rec(array, charactersArray, result)
+  }
+
+  switch (array[0]) {
+    case 'a':
+      console.log('Letra A')
+      character = 'a'
+      token = characterToEncryptedToken[character]
+      break
+
+    case 'e':
+      character = 'e'
+      token = characterToEncryptedToken[character]
+      break
+
+    case 'i':
+      character = 'i'
+      token = characterToEncryptedToken[character]
+      break
+
+    case 'o':
+      character = 'o'
+      token = characterToEncryptedToken[character]
+      break
+
+    case 'u':
+      character = 'u'
+      token = characterToEncryptedToken[character]
+      break
+  }
+  letterAndSplice = getLetterAndSplice([...array], token, character)
+  result += letterAndSplice.character
+  array.splice(0, letterAndSplice.splice)
+  return rec(array, charactersArray, result)
+}
+
+function getLetterAndSplice(array, token, letter) {
+  let character = ''
+  let splice = 1
+  let aux = array.splice(0, token.length)
+
+  if (aux.join('') === token) {
+    character = letter
+    splice = token.length
+  } else {
+    character = aux[0]
+    console.log(aux[0])
+  }
+  return { character, splice }
+}
+
+/*
 function decrypt(hashMessage = 'maiimesrobern vimeslenterlai') {
+  console.time('TempoExecucao')
+
   document.getElementById('form').addEventListener('submit', function (event) {
     event.preventDefault()
   })
 
   let result = ''
+  let aux = ''
   let palavra = hashMessage.split('')
   let arrayAuxiliar = []
-  let aux = ''
 
   while (true) {
     if (palavra.length === 0) {
@@ -97,104 +202,6 @@ function decrypt(hashMessage = 'maiimesrobern vimeslenterlai') {
     }
   }
 
-  console.log('result: ' + result)
-  console.log('FIm')
+  console.timeEnd('TempoExecucao')
 }
-
-//maiimesrobern vimeslenterlai
-function handleDecryptRecursivo(hashMessage = 'maiimesrobern vimeslenterlai') {
-  document.getElementById('form').addEventListener('submit', function (event) {
-    event.preventDefault()
-  })
-
-  const letterArray = ['a', 'e', 'i', 'o', 'u']
-  let palavra = hashMessage.split('')
-  const result = rec(palavra, letterArray, '')
-  console.log('resultado: ' + result)
-}
-
-function rec(array, caractersCifrados, result) {
-  let arrayAuxiliar = [...array]
-
-  if (array.length === 0) {
-    return result
-  }
-
-  if (!caractersCifrados.includes(array[0])) {
-    result += array[0]
-    array.splice(0, 1)
-    return rec(array, caractersCifrados, result)
-  }
-
-  switch (array[0]) {
-    case 'a':
-      aux = arrayAuxiliar.splice(0, 2)
-      if (aux.join('') === 'ai') {
-        result += 'a'
-        array.splice(0, aux.length)
-        return rec(array, caractersCifrados, result)
-      } else {
-        result = result + array[0]
-        array.splice(0, 1)
-        return rec(array, caractersCifrados, result)
-      }
-
-    case 'e':
-      aux = arrayAuxiliar.splice(0, 5)
-      if (aux.join('') === 'enter') {
-        result += 'e'
-        array.splice(0, aux.length)
-        return rec(array, caractersCifrados, result)
-      } else {
-        result = result + array[0]
-        array.splice(0, 1)
-        return rec(array, caractersCifrados, result)
-      }
-    case 'i':
-      aux = arrayAuxiliar.splice(0, 4)
-      if (aux.join('') === 'imes') {
-        result += 'i'
-        array.splice(0, aux.length)
-        return rec(array, caractersCifrados, result)
-      } else {
-        result = result + array[0]
-        array.splice(0, 1)
-        return rec(array, caractersCifrados, result)
-      }
-    case 'o':
-      aux = arrayAuxiliar.splice(0, 4)
-      console.log('Entrada: ' + array)
-      if (aux.join('') === 'ober') {
-        result += 'o'
-        array.splice(0, aux.length)
-        return rec(array, caractersCifrados, result)
-      } else {
-        result = result + array[0]
-        array.splice(0, 1)
-        return rec(array, caractersCifrados, result)
-      }
-
-    case 'u':
-      aux = arrayAuxiliar.splice(0, 4)
-      console.log('Entrada: ' + array)
-      if (aux.join('') === 'ufat') {
-        result += 'u'
-        array.splice(0, aux.length)
-        return rec(array, caractersCifrados, result)
-      } else {
-        result = result + array[0]
-        array.splice(0, 1)
-        return rec(array, caractersCifrados, result)
-      }
-  }
-}
-
-function hiddenElementById(id) {
-  const element = document.getElementById(id)
-  element.classList.add('hidden')
-}
-
-function showElementById(id) {
-  const element = document.getElementById(id)
-  element.classList.remove('hidden')
-}
+*/
